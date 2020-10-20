db39ae0a6dc96bfbc4fe527218681e53113187f2
David Faizulaev <maysam815@gmail.com>
2020-10-20 09:56:54 +0300

    work on ui

--- ui/exersice.js
+++ ui/exersice.js
@@ -1,36 +1,15 @@
-
 const baseURL = 'http://localhost:3000/download/'
 const myFetch = (id) => {
-  fetch(baseURL + id)
-  .then(response => response.body)
-  .then(rs => {
-    const reader = rs.getReader();
-
-    return new ReadableStream({
-      async start(controller) {
-        while (true) {
-          const { done, value } = await reader.read();
-
-          // When no more data needs to be consumed, break the reading
-          if (done) {
-            break;
-          }
-
-          // Enqueue the next data chunk into our target stream
-          controller.enqueue(value);
-        }
-
-        // Close the stream
-        controller.close();
-        reader.releaseLock();
-      }
-    })
-  })
-  .catch((error) => {
-    console.error(error);
-  })
-
-  return;
+    var element = document.createElement('a');
+    element.setAttribute('href', baseURL + id);
+    element.setAttribute('download', 'zip1.zip');
+  
+    element.style.display = 'none';
+    document.body.appendChild(element);
+  
+    element.click();
+  
+    document.body.removeChild(element);
 }
 
 document.addEventListener('click', event => {
